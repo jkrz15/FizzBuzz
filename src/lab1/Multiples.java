ca3bb38201398ce242cd4c344620b062bf12f193
JK <janica.krznaric@mail.utoronto.ca>
2026-09-15 15:41:29 -0400

    add helper function

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -4,13 +4,16 @@ public class Multiples {
     public static void main(String[] args) {
         // number of integers below 1000 that are positive and
         // divisible by 3 or 5
-        int num = 1000;
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int num, int a, int b) {
         int count = 0;
-        for (int i=1; i < num; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        for (int i = 1; i < num; i++) {
+            if (i % a == 0 || i % b == 0) {
                 count += 1;
             }
         }
-        System.out.println(count);
+        return count;
     }
 }
